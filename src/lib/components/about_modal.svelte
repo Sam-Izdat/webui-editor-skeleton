ee6eed3ba1bdb167feeb966729a071168cf90aec
Sam-Izdat <ghsamizdat@gmail.com>
2024-10-22 01:15:54 -0500

    add modal component

---
+++ src/lib/components/about_modal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	// Stores
+	import { getModalStore } from '@skeletonlabs/skeleton';
+
+	// Props
+	/** Exposes parent props to this component. */
+	export let parent: SvelteComponent;
+
+	const modalStore = getModalStore();
+
+	const cButton = 'fixed top-4 right-4 z-50 font-bold shadow-xl';
+	const cImage = 'max-w-[90%] max-h-[90%] rounded-container-token overflow-hidden float-left m-5';
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+	const cHeader = 'text-2xl font-bold';
+</script>
+
+
+{#if $modalStore[0]}
+	<div class="{cBase}">
+		<header class={cHeader}>{$modalStore[0].title ?? '(title missing)'}</header>
+		<img src='./icons/icon-128.png' class={cImage} alt="About" />
+		<article>{$modalStore[0].body ?? '(body missing)'}</article>
+		<footer class="modal-footer {parent.regionFooter}">
+        <button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>Close</button>
+    </footer>
+	</div>
+{/if}

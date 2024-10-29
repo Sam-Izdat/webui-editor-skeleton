d2fd509fdeef7fb7c63e10fcb87dc03677825317
Sam-Izdat <ghsamizdat@gmail.com>
2024-10-28 23:41:39 -0500

    svelte config error

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
   	prerender: {
 			crawl: true,
 			entries: process.env.BUILD_TYPE === 'static' 
-				? ['/get-gist/0', '/get-url/0'] // static routes here
+				? ['/gist/0', '/url/0'] // static routes here
 				: [] // leave empty dynamic routes
 		},
     paths: {

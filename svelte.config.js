cd4f470f7c115d0136c62db149c1b82cd935406a
Sam-Izdat <ghsamizdat@gmail.com>
2024-10-29 10:53:42 -0500

    add static route

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
   	prerender: {
 			crawl: true,
 			entries: process.env.BUILD_TYPE === 'static' 
-				? ['/', '/get-gist', '/get-url', '/gist/0', '/url/0'] // static routes here
+				? ['/', 'open-files', '/get-gist', '/get-url', '/gist/0', '/url/0'] // static routes here
 				: [] // leave empty dynamic routes
 		},
     paths: {
